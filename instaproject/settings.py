6779cdb89537e892e7fe349191ba65402226d14e
Joselyne97 <joselynejojo740@gmail.com>
2019-10-18 18:47:23 +0200

    I added the settings info

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = 'instaproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,7 +101,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
+            
         },
     },
 ]
@@ -109,13 +111,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'instaproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'myDB',
+#         'USER': 'alex',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
 
