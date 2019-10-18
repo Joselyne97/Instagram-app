d3bae2ee63177c3b85c8dc31961ddd17eb71e3f7
Joselyne97 <joselynejojo740@gmail.com>
2019-10-18 18:44:31 +0200

    I added the urls

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,1 +1,13 @@
-url(r'^$', views.feeds_photos, name = 'feeds'),
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url(r'^$', views.welcome, name="welcome"),
+    url(r'^picture/(\d+)',views.picture, name ='picture'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
