9a10bdc35b4959f5efb1898d966236d4c703df6f
Joselyne97 <joselynejojo740@gmail.com>
2019-10-18 18:45:35 +0200

    I added the users info

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,15 +1,8 @@
 from django import forms
-from .models import Photo
+from .models import Profile
 
 
 class InstagramLetterForm(forms.Form):
     your_username = forms.CharField(label='Your Username',max_length=30)
     email = forms.EmailField(label='Email')
 
-class NewPhotoForm(forms.modelForm):
-    class Meta:
-        model = Photo
-        # exclude = ['']
-    widgets = {
-            'tags': forms.CheckboxSelectMultiple(),
-        }    
