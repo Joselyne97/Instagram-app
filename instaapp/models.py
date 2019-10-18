1ab6efd42ff618aea731f696470516612091c739
Joselyne97 <joselynejojo740@gmail.com>
2019-10-18 18:44:00 +0200

    I added the database table

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,11 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Profile(models.Model):
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=150)
-    password = models.CharField(max_length=100)
 
-class Photo(models.Model):
-    photo
+    # user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(max_length=250, null=True, blank=True, default='bio')
+    profile_pic = models.ImageField(upload_to = 'pic/', blank=True, null=True)
+    user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    # @classmethod
+
+    def __str__(self):
+        return self.user.username
+
+
+class Image(models.Model):
+    image=models.ImageField(upload_to='pic/',)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,)
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
+    caption=models.TextField()
+    # likes=models.IntegerField(default=0)
+    # comment=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.caption
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+#     @classmethod
+#     def get_image_by_id(cls, tags):
+#         images = cls.objects.get(pk=id)
+#         return images
+
+#     @classmethod
+#     def filter_by_tag(cls, tags):
+#         images = cls.
+
