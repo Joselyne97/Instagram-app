841eff64c48d95e7167ec92c346e00ddacced914
Joselyne97 <joselynejojo740@gmail.com>
2019-10-21 20:35:25 +0200

    I added the classes

--- instaapp/models.py
+++ instaapp/models.py
@@ -5,22 +5,28 @@ from tinymce.models import HTMLField
 
 class Profile(models.Model):
 
-    bio = models.TextField(max_length=250, null=True, blank=True, default='bio')
+    bio = HTMLField()
     profile_pic = models.ImageField(upload_to = 'pic/', blank=True, null=True)
     user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
     
+    # @classmethod
+    # def get_all_id(cls)
+    #     profile = Profile.objects.get(user=id)
+    #     return profile
 
+    @classmethod
+    def update_profile(cls,id,value):
+        cls.objects.filter(id=id).update(user_id = new_user)
 
+    
     def save_profile(self):
         self.save()
 
     def delete_profile(self):
         self.delete()
 
-    # @classmethod
-
     def __str__(self):
-        return self.username
+        return self.user
 
     @classmethod
     def search_by_profile(cls,username):
@@ -31,18 +37,18 @@ class Profile(models.Model):
 class Image(models.Model):
     image=models.ImageField(upload_to='pic/',)
     image_name = models.CharField(max_length =60)
-    user = models.ForeignKey(User,related_name = 'username', on_delete=models.CASCADE, blank=True,)
-    profile = models.ForeignKey(User,related_name = 'username', on_delete=models.CASCADE, blank=True)
-    caption=models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,)
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
+    
+    caption=HTMLField()
     # likes=models.ManyToManyField(User, on_delete=models.CASCADE, related_name = 'likes', blank=True)
     # comment=models.TextField(blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
-    
-
     @classmethod
     def save_image(self):
+
         self.save()
 
     @classmethod
@@ -54,27 +60,8 @@ class Image(models.Model):
         caption=cls.objects.filter(caption_id=id).update(caption=caption)
         return caption
 
-    # def total_likes(self)
-    #     self.likes.count()
+    
 
     def __str__(self):
         return self.caption
-
-
-class Follow(models.Model):
-    following=models.ForeignKey(User, on_delete=models.CASCADE, related_name='rel_from_set')
-    follower=models.ForeignKey(User, on_delete=models.CASCADE, related_name='rel_to_set')
-
-    # @classmethod
-    # def save_following(self):
-    #     self.save()
-
-    # @classmethod
-    # def delete_follower(self):
-    #     self.delete()
-
-    def __str__(self):
-        return '{} follows {}'.format(self.user_to)
-
-# User.add_to_class('following', models.ManyToManyField('self', throuh=Follow, related_name='followers',symmetrical=False))
 
