39f1fc3133efba8777c4b2f0bd65064e91900d55
Joselyne97 <joselynejojo740@gmail.com>
2019-10-21 09:31:46 +0200

    I added the urls for updating profile

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -6,8 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.welcome, name="welcome"),
-    url(r'^picture/(\d+)',views.picture, name ='picture'),
-    
+    url(r'^picture',views.picture, name ='picture'),
+    url(r'^new/picture',views.new_picture, name ='new-picture'),
+    url(r'^profile/(\d+)', views.user_profile, name='user-profile'),
+    url(r'^editprofile$', views.edit_profile, name="edit-profile"),
+    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^follow/(\d+)',views.follow, name='follow'),
+
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
