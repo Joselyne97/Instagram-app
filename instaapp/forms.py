8a2930f10544acc343a939f56a7e381979e9e023
Joselyne97 <joselynejojo740@gmail.com>
2019-10-21 09:29:42 +0200

    I added the form for updating profile and images

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import Profile
+from .models import Profile,Image
+from django.contrib.auth.models import User
 
 
-class InstagramLetterForm(forms.Form):
-    your_username = forms.CharField(label='Your Username',max_length=30)
-    email = forms.EmailField(label='Email')
+class NewImageForm(forms.Form):
+    class Meta:
+        model = Image
+        exclude = [ 'image_name', 'user', 'profile']   
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields=['bio','profile_pic']
+        exclude =['user']
 
